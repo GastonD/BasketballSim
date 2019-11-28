aeab6ac3a8469311b4f610dc211035e5527032ff
Gaston Fernando Davola <gaston.davola.sp@SofttekProjects.com>
2019-11-28 16:38:30 -0300

    Simulacion de Partido

--- Program.cs
+++ Program.cs
@@ -18,10 +18,26 @@ namespace BasketballSim
             Console.WriteLine(t2.getName());
             Console.WriteLine("Ofensiva: " + t2.getOffRtg());
             Console.WriteLine("Defensiva: " + t2.getDefRtg());
+
+            playGame(t1, t2);
             
+        }
+
+        public static void playGame(Team t1, Team t2){
+            int t1Score = 0;
+            int t2Score = 0;
+
+            if (t1Score < 0) t1Score = 0;
 
 
-            
+            t1Score = t1.getOffRtg() - t2.getDefRtg();
+            if (t1Score < 0) t1Score = 0;
+            t2Score = t2.getOffRtg() - t1.getDefRtg();
+            if (t2Score < 0) t2Score = 0;
+
+            Console.WriteLine("Score: " + t1.getName() + ": " + t1Score + " - " + t2.getName() + ": " + t2Score);
+
+        
         }
     }
 }
