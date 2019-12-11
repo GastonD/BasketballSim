f7cb2516f5472c9fd9a301bfb75ce084b72a75f8
Gaston Fernando Davola <gaston.davola.sp@SofttekProjects.com>
2019-12-11 16:00:54 -0300

    Menu Principal

--- Program.cs
+++ Program.cs
@@ -8,19 +8,29 @@ namespace BasketballSim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenido a Basketball Simulator!");
-            Console.WriteLine("");
+            bool showMenu = true;
+            while(showMenu){
+                showMenu = MainMenu();
+            }
+            
+        }
 
+        public static void playDay(int Day){
+            List<Game> gamesToPlay = new List<Game>();
+            gamesToPlay = MyLeague.Instance.gamesInDay(Day);
+
+            foreach(Game g in gamesToPlay){
+                g.playGame();
+                //Console.ReadKey(); 
+            }
+        }
+
+        private static void PlayLeague(){
             DataManager.Instance.createTeams();
             DataManager.Instance.populateTeams();
-
-            Console.ReadKey();
-
             MyLeague.Instance.setUpSeason();
 
             int daysPlayed = 0;
-
-            //Console.ReadKey();
 
             for (int i = 0; i<MyLeague.Instance.getLeagueDays();i++){
                 Console.WriteLine("Día {0}",i+1);
@@ -38,16 +48,30 @@ namespace BasketballSim
             MyLeague.Instance.displayTeamRecords();
             MyLeague.Instance.getTopScorer();
             MyLeague.Instance.nextYear();
-            
+
+            Console.ReadKey();
         }
 
-        public static void playDay(int Day){
-            List<Game> gamesToPlay = new List<Game>();
-            gamesToPlay = MyLeague.Instance.gamesInDay(Day);
+        private static bool MainMenu(){
+            Console.Clear();
+            Console.WriteLine("Bienvenido a Basketball Simulator!");
+            Console.WriteLine("1) Jugar Partido Amistoso");
+            Console.WriteLine("2) Simular Liga");
+            Console.WriteLine("3) Salir");
 
-            foreach(Game g in gamesToPlay){
-                g.playGame();
-                //Console.ReadKey(); 
+            switch(Console.ReadLine()){
+                case "1":
+                return true;
+
+                case "2":
+                PlayLeague();
+                return true;
+
+                case "3":
+                return false;
+
+                default:
+                return true;
             }
         }
 
